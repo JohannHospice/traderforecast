e1ffe7316c3b774fb090d05b383ab2f55e20c654
Johann Hospice <johann.hospice@ext.vallourec.com>
2024-03-13 23:52:01 +0100

    refactor(lightweight-charts): lightweight-charts options creation

--- apps/web/styles/lightweight-charts-options.ts
+++ apps/web/styles/lightweight-charts-options.ts
@@ -1,49 +1,52 @@
 'use client';
 import { ColorType, DeepPartial, TimeChartOptions } from 'lightweight-charts';
 
-export const OPTIONS_LIGHT: DeepPartial<TimeChartOptions> = {
-  layout: {
-    background: {
-      type: ColorType.Solid,
-      color: 'hsl(0 0% 100%)',
+function createTimeChartOptions({
+  bgColor,
+  textColor,
+  lineColor,
+  scaleColor,
+}: {
+  bgColor: string;
+  textColor: string;
+  lineColor: string;
+  scaleColor: string;
+}): DeepPartial<TimeChartOptions> {
+  return {
+    layout: {
+      background: {
+        type: ColorType.Solid,
+        color: bgColor,
+      },
+      textColor: textColor,
     },
-    textColor: 'hsl(224 71.4% 4.1%)',
-  },
-  grid: {
-    vertLines: {
-      color: 'hsl(220 13% 91%)',
+    grid: {
+      vertLines: {
+        color: lineColor,
+      },
+      horzLines: {
+        color: lineColor,
+      },
     },
-    horzLines: {
-      color: 'hsl(220 13% 91%)',
+    timeScale: {
+      borderColor: scaleColor,
     },
-  },
-  timeScale: {
-    borderColor: 'hsl(220 13% 91%)',
-  },
-  rightPriceScale: {
-    borderColor: 'hsl(220 13% 91%)',
-  },
-};
-export const OPTIONS_DARK: DeepPartial<TimeChartOptions> = {
-  layout: {
-    background: {
-      type: ColorType.Solid,
-      color: 'hsl(224 71.4% 4.1%)',
+    rightPriceScale: {
+      borderColor: scaleColor,
     },
-    textColor: 'hsl(210 20% 98%)',
-  },
-  grid: {
-    vertLines: {
-      color: 'hsl(180 2.86% 6.86%)',
-    },
-    horzLines: {
-      color: 'hsl(180 2.86% 6.86%)',
-    },
-  },
-  timeScale: {
-    borderColor: 'hsl(215 27.9% 16.9%)',
-  },
-  rightPriceScale: {
-    borderColor: 'hsl(215 27.9% 16.9%)',
-  },
-};
+  };
+}
+
+export const OPTIONS_LIGHT = createTimeChartOptions({
+  bgColor: 'transparent',
+  textColor: 'hsl(224 71.4% 4.1%)',
+  lineColor: 'hsl(220 13% 91%)',
+  scaleColor: 'hsl(220 13% 91%)',
+});
+
+export const OPTIONS_DARK = createTimeChartOptions({
+  bgColor: 'transparent',
+  textColor: 'hsl(210 20% 98%)',
+  lineColor: 'hsl(180 2.86% 6.86%)',
+  scaleColor: 'hsl(215 27.9% 16.9%)',
+});
