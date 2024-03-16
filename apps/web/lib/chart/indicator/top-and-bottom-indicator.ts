40be429cac650d3d51be11c6ea5706ae62650c60
Johann Hospice <johann.hospice@ext.vallourec.com>
2024-03-16 11:29:44 +0100

    feat(indicator): working on top bottom

--- apps/web/lib/chart/indicator/top-and-bottom-indicator.ts
+++ apps/web/lib/chart/indicator/top-and-bottom-indicator.ts
@@ -1,39 +1,14 @@
 import { Indicator, IndicatorResult, Marker } from '.';
+import { BottomIndicator } from './bottom-indicator';
+import { TopMarkersIndicator } from './top-indicator';
 
 export class TopAndBottomIndicator implements Indicator {
   execute(klines: Kline[]): IndicatorResult {
-    const markers: Marker[] = [];
-
-    const significatKlines = [klines[0]];
-
-    for (let i = 1; i < klines.length - 1; i++) {
-      const current = klines[i];
-      const previous = significatKlines[significatKlines.length - 1];
-
-      const isHigh = current.high > previous.high;
-      const isLow = current.low < previous.low;
-
-      if (isHigh) {
-        significatKlines.push(current);
-        markers.push({
-          time: current.closeTime,
-          color: 'green',
-          position: 'aboveBar',
-          shape: 'arrowDown',
-        });
-      }
-
-      if (isLow) {
-        significatKlines.push(current);
-        markers.push({
-          time: current.closeTime,
-          color: 'red',
-          position: 'belowBar',
-          shape: 'arrowUp',
-        });
-      }
-    }
-
-    return { markers };
+    return {
+      markers: [
+        new TopMarkersIndicator().execute(klines).markers || [],
+        new BottomIndicator().execute(klines).markers || [],
+      ].flat(),
+    };
   }
 }
--- apps/web/lib/chart/indicator/top-indicator.ts
+++ apps/web/lib/chart/indicator/top-indicator.ts
@@ -1,19 +1,43 @@
-import { Indicator, IndicatorResult } from '.';
+import { Indicator, IndicatorResult, Marker } from '.';
 
 export class TopMarkersIndicator implements Indicator {
   execute(klines: Kline[]): IndicatorResult {
-    const markers = [];
-    let max = Number.NEGATIVE_INFINITY;
-    for (let i = 0; i < klines.length; i++) {
-      const kline = klines[i];
-      if (kline.high > max) {
-        max = kline.high;
+    const markers: Marker[] = [];
+    const klineRegionSize = 1;
+
+    for (let i = klineRegionSize; i < klines.length; i++) {
+      // const next = klines[i];
+      let isTop = true;
+
+      // a value of Math.ceil(klineRegionSize / 2)
+      const part = Math.ceil(klineRegionSize / 2);
+
+      const current = klines[i - part];
+      for (let j = -part; j < part; j++) {
+        if (j === 0) {
+          continue;
+        }
+        if (isTop === false) {
+          break;
+        }
+        const next = klines[i + j];
+        console.log(next);
+
+        isTop = current.close > next.close;
+      }
+
+      if (isTop) {
         markers.push({
-          time: kline.closeTime,
-          color: 'red',
+          time: current.closeTime,
+          color: 'green',
+          position: 'aboveBar',
+          shape: 'arrowDown',
         });
       }
     }
-    return { markers };
+
+    return {
+      markers: markers,
+    };
   }
 }
