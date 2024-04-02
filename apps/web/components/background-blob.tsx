6ec1c168c22a8c6f131ce1b3d7f810abfa02a371
Johann Hospice <johann.hospice@ext.vallourec.com>
2024-04-02 20:08:12 +0200

    style(blob): less transparent background

--- apps/web/components/background-blob.tsx
+++ apps/web/components/background-blob.tsx
@@ -6,7 +6,7 @@ export function BackgroundBlob() {
           animate-blob
           rounded-[100%] h-[50vh] w-[50vh] 
           bg-gradient-to-t from-red-500 via-orange-500 to-blue-500
-          opacity-5 sm:opacity-10
+          opacity-10 sm:opacity-50
           '
       />
     </div>
