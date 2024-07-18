d4a9de93e29a1ab8f5ed492135268e2a5e342a6c
Johann Hospice <johannhospice.dev@gmail.com>
2024-07-18 14:52:49 +0200

    feat(change-dependencies): using local market only when required in env vars

--- apps/web/lib/api/dependancies/index.ts
+++ apps/web/lib/api/dependancies/index.ts
@@ -1,15 +1,16 @@
 import { MarketRepository } from './market/repositories';
 import { SantimentMarketRepository } from './market/repositories/santiment-market-repository';
-import { CachedMarketRepository } from './market/repositories/cached-market-repository';
 import { appoloSantiment } from './market/clients/appolo-santiment';
 import { LocalMarketRepository } from './market/repositories/local-market-repository';
 
+// const cachedMarket = new CachedMarketRepository(
+//   new SantimentMarketRepository(appoloSantiment)
+// );
+
 export default {
-  cachedMarket: new CachedMarketRepository(
-    new SantimentMarketRepository(appoloSantiment)
-  ),
-  market: new LocalMarketRepository(),
+  market: process.env.OFFLINE
+    ? new LocalMarketRepository()
+    : new SantimentMarketRepository(appoloSantiment),
 } satisfies {
-  cachedMarket: MarketRepository;
   market: MarketRepository;
 };
